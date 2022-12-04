ac501c9205dc69e3da7a908e56f61d818ae01d9a
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-04 16:59:53 +0200

    Ready for day 5

---
+++ day5/src/main.rs
@@ -0,0 +1,9 @@
+use aoc::read_input_lines;
+use itertools::Itertools;
+use std::collections::HashSet;
+
+fn main() {
+    read_input_lines()
+    .iter()
+    .for_each(|line| println!("{line}"));
+}
