9d4f609ed363b1ecd8624248f5b91dd7fa9e2b91
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-04 16:57:01 +0200

    Day 4 cleaned

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,32 +1,40 @@
 use aoc::read_input_lines;
+use itertools::Itertools;
 use std::collections::HashSet;
 
 fn main() {
-    let matches: Vec<(bool, bool)> = read_input_lines()
+    let sets = read_input_lines()
         .iter()
-        .map(|line| check_subset(line))
-        .collect();
+        .map(|line| line_to_sets(line))
+        .collect::<Vec<(HashSet<u32>, HashSet<u32>)>>();
 
-    let part1 = matches.iter().filter(|(b, _)| *b).count();
+    let part1 = sets
+        .iter()
+        .filter(|pair| pair.0.is_subset(&pair.1) || pair.0.is_superset(&pair.1))
+        .count();
     println!("Part 1: {part1}");
 
-    let part2 = matches.iter().filter(|(_, b)| *b).count();
+    let part2 = sets
+        .iter()
+        .filter(|pair| !pair.0.is_disjoint(&pair.1))
+        .count();
     println!("Part 2: {part2}");
 }
 
-fn check_subset(line: &str) -> (bool, bool) {
-    let sets = line.split(',').map(to_set).collect::<Vec<HashSet<u32>>>();
-    (
-        sets[0].is_subset(&sets[1]) || sets[0].is_superset(&sets[1]),
-        !sets[0].is_disjoint(&sets[1]),
-    )
+fn line_to_sets(line: &str) -> (HashSet<u32>, HashSet<u32>) {
+    line.split(',')
+        .map(to_set)
+        .into_iter()
+        .collect_tuple()
+        .expect("More than two elves in a group")
 }
 
 fn to_set(s: &str) -> HashSet<u32> {
-    let ends = s
+    let (start, end) = s
         .split('-')
         .map(|c| c.parse::<u32>().unwrap())
-        .collect::<Vec<u32>>();
+        .collect_tuple()
+        .expect("More than two numbers in a range");
 
-    (ends[0]..=ends[1]).collect()
+    (start..=end).collect()
 }
