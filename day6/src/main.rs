eb36c41cf75b2850afb59c0969aa88a75f6166f9
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-06 10:16:17 +0200

    Day 6 cleaned

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,18 +1,41 @@
 use std::collections::HashSet;
 
 fn main() {
-    println!("Part 1: {}", find_marker(4));
-    println!("Part 2: {}", find_marker(14));
+    let input = aoc::read_input_string();
+    println!("Part 1: {}", find_marker(4, &input));
+    println!("Part 2: {}", find_marker(14, &input));
 }
 
-fn find_marker(n: usize) -> usize {
-    aoc::read_input_string()
+fn find_marker(n: usize, input: &str) -> usize {
+    input
         .chars()
         .collect::<Vec<_>>()
         .windows(n)
         .enumerate()
-        .find(|(_, window)| HashSet::<char>::from_iter(window.iter().copied()).len() == n)
+        .find(|(_, window)| HashSet::<_>::from_iter(window.iter().copied()).len() == n)
+        .map(|(i, _)| i + n)
         .unwrap()
-        .0
-        + n
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn find_marker_4() {
+        assert_eq!(find_marker(4, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 7);
+        assert_eq!(find_marker(4, "bvwbjplbgvbhsrlpgdmjqwftvncz"), 5);
+        assert_eq!(find_marker(4, "nppdvjthqldpwncqszvftbrmjlhg"), 6);
+        assert_eq!(find_marker(4, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
+        assert_eq!(find_marker(4, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+    }
+
+    #[test]
+    fn find_marker_14() {
+        assert_eq!(find_marker(14, "mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+        assert_eq!(find_marker(14, "bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+        assert_eq!(find_marker(14, "nppdvjthqldpwncqszvftbrmjlhg"), 23);
+        assert_eq!(find_marker(14, "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+        assert_eq!(find_marker(14, "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
+    }
 }
