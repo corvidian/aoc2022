b794411db42b1df190e8c3ba0c8b0f629133a3b9
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-12 14:27:41 +0200

    Day 12 Clean up and path visualization

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -7,56 +7,66 @@ fn main() {
         .map(|i| i.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
 
-    let starts: Vec<(usize,usize)> = map
-        .iter()
-        .enumerate()
-        .flat_map(|(i, line)| {
-            line.iter()
-                .enumerate()
-                .filter(|(_, &c)| c == 'S' || c == 'a')
-                .map(move |(pos, _)| (i, pos))
-        })
-        .collect();
-    let part1_start = starts
-        .iter()
-        .find(|&start| map[start.0][start.1] == 'S')
-        .unwrap();
-    map[part1_start.0][part1_start.1] = 'a';
+    let mut starts: Vec<(usize, usize)> = vec![];
+    let mut part1_start = (0, 0);
+    let mut target = (0, 0);
+
+    for (y, row) in map.iter_mut().enumerate() {
+        for (x, char) in row.iter_mut().enumerate() {
+            match char {
+                'S' => {
+                    part1_start = (y, x);
+                    starts.push((y, x));
+                    *char = 'a';
+                }
+                'E' => {
+                    target = (y, x);
+                    *char = 'z';
+                }
+                'a' => starts.push((y, x)),
+                _ => {}
+            }
+        }
+    }
 
     let min = starts
         .iter()
-        .filter_map(|&start| find(&map, start))
-        .min()
-        .unwrap();
+        .filter_map(|&start| find(&map, start, target, false))
+        .min();
 
-    println!("Part 1: {:?}", find(&map, *part1_start));
+    println!("Part 1: {:?}", find(&map, part1_start, target, true));
     println!("Part 2: {min:?}");
 }
 
-fn find(map: &[Vec<char>], start: (usize,usize)) -> Option<i32> {
+fn find(
+    map: &[Vec<char>],
+    start: (usize, usize),
+    target: (usize, usize),
+    visualize: bool,
+) -> Option<i32> {
     let height = map.len();
     let width = map[0].len();
 
     let mut visited = vec![vec![i32::MAX; width]; height];
     visited[start.0][start.1] = 0;
 
-    let mut queue: VecDeque<(usize,usize)> = vec![start].into();
+    let mut queue: VecDeque<(usize, usize)> = vec![start].into();
 
-    while let Some(current) = queue.pop_front() {
-        let steps = visited[current.0][current.1];
-        let current_value = map[current.0][current.1] as u32;
-        if map[current.0][current.1] == 'E' {
+    while let Some((x, y)) = queue.pop_front() {
+        let steps = visited[x][y];
+        if (x, y) == target {
+            if visualize {
+                visualize_visited(map, &visited, target);
+            }
             return Some(steps);
         }
+        let current_value = map[x][y] as u32;
 
-        for delta in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
-            if let Ok(row) = usize::try_from(current.0 as i32 + delta.0) {
-                if let Ok(col) = usize::try_from(current.1 as i32 + delta.1) {
+        for (dy, dx) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            if let Ok(row) = usize::try_from(x as i32 + dx) {
+                if let Ok(col) = usize::try_from(y as i32 + dy) {
                     if row < height && col < width {
-                        let mut next_value = map[row][col] as u32;
-                        if map[row][col] == 'E' {
-                            next_value = 'z' as u32
-                        }
+                        let next_value = map[row][col] as u32;
                         if next_value <= current_value + 1
                             && visited[row][col] > steps
                             && !queue.contains(&(row, col))
@@ -69,5 +79,48 @@ fn find(map: &[Vec<char>], start: (usize,usize)) -> Option<i32> {
             }
         }
     }
-    return None;
+    None
+}
+
+fn visualize_visited(map: &[Vec<char>], visited: &[Vec<i32>], target: (usize, usize)) {
+    let mut output: Vec<_> = map.to_vec();
+
+    let (mut y, mut x) = target;
+    output[y][x] = '🔰';
+
+    while visited[y][x] > 0 {
+        if y > 0 && visited[y - 1][x] as u32 == visited[y][x] as u32 - 1 {
+            output[y - 1][x] = '⏬';
+            y -= 1;
+        } else if x > 0 && visited[y][x - 1] as u32 == visited[y][x] as u32 - 1 {
+            output[y][x - 1] = '⏩';
+            x -= 1;
+        } else if y < visited.len() && visited[y + 1][x] as u32 == visited[y][x] as u32 - 1 {
+            output[y + 1][x] = '⏫';
+            y += 1;
+        } else if x < visited[y].len() && visited[y][x + 1] as u32 == visited[y][x] as u32 - 1 {
+            output[y][x + 1] = '⏪';
+            x += 1;
+        }
+    }
+
+    output[y][x] = '⛳';
+
+    let narrow_a = 'a' as u32;
+    let fullwidth_a = '\u{FF21}' as u32;
+
+    for row in output {
+        for char in row {
+            let char_value = char as u32 - narrow_a;
+            if char_value < 26 {
+                print!("{}", char::try_from(fullwidth_a + char_value).unwrap());
+            } else {
+                print!("{char}");
+            }
+        }
+        println!();
+    }
+
+    println!();
+    println!();
 }
