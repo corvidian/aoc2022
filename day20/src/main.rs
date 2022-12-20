cc0a1a810b486fdfb1cab548bbea5f016c207d26
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-21 00:32:58 +0200

    Day 20 use slice rotations for speed

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,4 +1,3 @@
-use log::info;
 use std::fmt::Display;
 
 #[derive(Clone, Debug)]
@@ -25,7 +24,6 @@ impl Display for Item {
 const DECRYPT_KEY: i64 = 811589153;
 
 fn main() {
-    aoc::init_logging();
     let mut file: Vec<_> = aoc::input_lines()
         .enumerate()
         .map(|(i, value)| Item::new(i, value.parse::<i64>().unwrap()))
@@ -36,7 +34,7 @@ fn main() {
     result(&part1_buffer);
 
     for mut item in &mut file {
-        item.value = item.value * DECRYPT_KEY
+        item.value *= DECRYPT_KEY
     }
 
     for _ in 0..10 {
@@ -51,7 +49,7 @@ fn result(buffer: &Vec<Item>) {
     let two_thousand = &buffer[(pos_zero + 2000).rem_euclid(buffer.len())];
     let three_thousand = &buffer[(pos_zero + 3000).rem_euclid(buffer.len())];
 
-    info!(
+    println!(
         "{} + {} + {} = {}",
         thousand,
         two_thousand,
@@ -70,8 +68,12 @@ fn mix(buffer: &mut Vec<Item>) {
 fn shift(buffer: &mut Vec<Item>, i: usize) {
     let n = buffer.len();
     let moves = buffer[i].value.rem_euclid(n as i64 - 1) as usize;
-    for j in 0..moves {
-        buffer.swap((i + j).rem_euclid(n), (i + j + 1).rem_euclid(n))
+    let end = (i + moves).rem_euclid(n) + 1;
+    if i < end {
+        buffer[i..end].rotate_left(1)
+    }
+    if i > end {
+        buffer[end..=i].rotate_right(1);
     }
 }
 
@@ -87,7 +89,7 @@ mod tests {
     #[case(vec![1, 2, 3, -2, -3, 0, 4],2,vec![1, 2, -2, -3, 0, 3, 4])]
     #[case(vec![1, 2, -2, -3, 0, 3, 4],2,vec![1, 2, -3, 0, 3, 4, -2])]
     #[case(vec![1, 2, -3, 0, 3, 4, -2],3,vec![1, 2, -3, 0, 3, 4, -2])]
-    #[case(vec![1, 2, -3, 0, 3, 4, -2],5,vec![2, -3, 4, 0, 3, -2, 1])]
+    #[case(vec![1, 2, -3, 0, 3, 4, -2],5,vec![1, 2, -3, 4, 0, 3, -2])]
     fn case1(#[case] input: Vec<i64>, #[case] index: usize, #[case] output: Vec<i64>) {
         let mut buffer = input.iter().map(|i| Item::new(0, *i)).collect();
         shift(&mut buffer, index);
