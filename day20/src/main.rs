cc764e29f059a4b6ef31ce4f5ef01019b1f62a88
Topias Heinonen <topias.heinonen@gofore.com>
2022-12-21 00:08:14 +0200

    Day 20 Simple but slow swaps

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -1,11 +1,19 @@
-use itertools::Itertools;
-use log::{debug, info};
+use log::info;
 use std::fmt::Display;
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Debug)]
 struct Item {
     original_index: usize,
     value: i64,
+}
+
+impl Item {
+    fn new(original_index: usize, value: i64) -> Item {
+        Item {
+            original_index,
+            value,
+        }
+    }
 }
 
 impl Display for Item {
@@ -20,13 +28,8 @@ fn main() {
     aoc::init_logging();
     let mut file: Vec<_> = aoc::input_lines()
         .enumerate()
-        .map(|(i, value)| Item {
-            original_index: i,
-            value: value.parse::<i64>().unwrap(),
-        })
+        .map(|(i, value)| Item::new(i, value.parse::<i64>().unwrap()))
         .collect();
-
-    debug!("{}", file.iter().join(", "));
 
     let mut part1_buffer = file.clone();
     mix(&mut part1_buffer);
@@ -44,9 +47,9 @@ fn main() {
 
 fn result(buffer: &Vec<Item>) {
     let pos_zero = buffer.iter().position(|a| a.value == 0).unwrap();
-    let thousand = buffer[(pos_zero + 1000).rem_euclid(buffer.len())];
-    let two_thousand = buffer[(pos_zero + 2000).rem_euclid(buffer.len())];
-    let three_thousand = buffer[(pos_zero + 3000).rem_euclid(buffer.len())];
+    let thousand = &buffer[(pos_zero + 1000).rem_euclid(buffer.len())];
+    let two_thousand = &buffer[(pos_zero + 2000).rem_euclid(buffer.len())];
+    let three_thousand = &buffer[(pos_zero + 3000).rem_euclid(buffer.len())];
 
     info!(
         "{} + {} + {} = {}",
@@ -58,19 +61,36 @@ fn result(buffer: &Vec<Item>) {
 }
 
 fn mix(buffer: &mut Vec<Item>) {
-    let modulo = buffer.len() as i64 - 1;
     for i in 0..buffer.len() {
         let i = buffer.iter().position(|a| a.original_index == i).unwrap();
-        let a = buffer.remove(i);
+        shift(buffer, i);
+    }
+}
 
-        let new_i = (i as i64 + a.value).rem_euclid(modulo) as usize;
+fn shift(buffer: &mut Vec<Item>, i: usize) {
+    let n = buffer.len();
+    let moves = buffer[i].value.rem_euclid(n as i64 - 1) as usize;
+    for j in 0..moves {
+        buffer.swap((i + j).rem_euclid(n), (i + j + 1).rem_euclid(n))
+    }
+}
 
-        debug!(
-            "Index for {a} is {new_i} (= {i} + {} % {})",
-            a.value, modulo
-        );
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
 
-        buffer.insert(new_i, a);
-        debug!("{}", buffer.iter().map(|a| a.value).join(", "));
+    #[rstest]
+    #[case(vec![1, 2, -3, 3, -2, 0, 4],0,vec![2, 1, -3, 3, -2, 0, 4])]
+    #[case(vec![2, 1, -3, 3, -2, 0, 4],0,vec![1, -3, 2, 3, -2, 0, 4])]
+    #[case(vec![1, -3, 2, 3, -2, 0, 4],1,vec![1, 2, 3, -2, -3, 0, 4])]
+    #[case(vec![1, 2, 3, -2, -3, 0, 4],2,vec![1, 2, -2, -3, 0, 3, 4])]
+    #[case(vec![1, 2, -2, -3, 0, 3, 4],2,vec![1, 2, -3, 0, 3, 4, -2])]
+    #[case(vec![1, 2, -3, 0, 3, 4, -2],3,vec![1, 2, -3, 0, 3, 4, -2])]
+    #[case(vec![1, 2, -3, 0, 3, 4, -2],5,vec![2, -3, 4, 0, 3, -2, 1])]
+    fn case1(#[case] input: Vec<i64>, #[case] index: usize, #[case] output: Vec<i64>) {
+        let mut buffer = input.iter().map(|i| Item::new(0, *i)).collect();
+        shift(&mut buffer, index);
+        assert_eq!(buffer.iter().map(|i| i.value).collect::<Vec<_>>(), output)
     }
 }
